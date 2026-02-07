21112fe2481ede1a2a72738928b15c92e2c8c21a
Ryan Phyo <zawminp2003@gmail.com>
2026-02-07 02:01:58 +0000

    Google Map API Implementation

--- react_eventReg/src/pages/create-event-process/ScratchCreation.jsx
+++ react_eventReg/src/pages/create-event-process/ScratchCreation.jsx
@@ -1,20 +1,64 @@
 import { Box, Button, TextField, Typography, MenuItem, FormControl, FormLabel } from "@mui/material";
 import Navbar from "../navbar/Navbar";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import {APIProvider, Map, AdvancedMarker, Pin, useMapsLibrary} from "@vis.gl/react-google-maps";
+
 
 function ScratchCreation() {
   const [eventInfo, setEventInfo] = useState({
     event_title: "",
     description: "",
-    location: "",
+    location: {
+      address:"",
+      latitude: null,
+      longitude: null
+    },
+
     start_date: "",
-    end_date: "",
+    
     start_time: "",
     end_time: "",
     event_image_url: "",
-    slug: "",
-    status: "DRAFT",
+    
   });
+
+  const mapRef = useRef(null);
+  const inputRef = useRef(null);
+  const places = useMapsLibrary("places");
+
+  const [defaultLocationMarker, setDefaultLocationMarker] = useState({
+    lat: 51.752,
+    lng: -1.2577,
+  });
+
+ useEffect(() => {
+  if (!places || !inputRef.current) return; 
+
+  const autocomplete = new places.Autocomplete(inputRef.current, {
+    fields: ["geometry", "formatted_address"],
+  });
+
+  autocomplete.addListener("place_changed", () => {
+    const place = autocomplete.getPlace();
+    if (!place.geometry) return;
+
+    const lat = place.geometry.location.lat();
+    const lng = place.geometry.location.lng();
+
+    setDefaultLocationMarker({ lat, lng });
+    setEventInfo(prev => ({
+      ...prev,
+      location: {
+        address: place.formatted_address,
+        latitude: lat,
+        longitude: lng,
+      },
+    }));
+    mapRef.current?.panTo({ lat, lng });
+  });
+
+}, [places]);
+
 
   const handleChange = (e) => {
     setEventInfo({
@@ -25,11 +69,12 @@ function ScratchCreation() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(eventInfo); // send to backend later
+    console.log(eventInfo);
+   
   };
 
-  return (
-    <Box sx={{background: "linear-gradient(to top, #20002c, #cbb4d4)"}}>
+return (
+    <Box sx={{background: "linear-gradient(to top, #20002c, #cbb4d4)", }}>
       
 
       <Box
@@ -39,6 +84,7 @@ function ScratchCreation() {
           p: "40px",
           justifyContent: "center",
           alignItems: "center",
+          height: "auto"
         }}
       >
         
@@ -49,48 +95,138 @@ function ScratchCreation() {
             backgroundColor: "#fff",
             padding: 4,
             borderRadius: 2,
-            width: "400px",
+            width: "650px",
             display: "flex",
             flexDirection: "column",
-            gap: 2,
+            gap: 4,
           }}
         >
-          <Typography variant="h6" textAlign="center">
+          {/* <Typography variant="h6" textAlign="center">
             Create Event
-          </Typography>
+          </Typography> */}
 
-          
-            <FormLabel sx={{color: "black"}}>Event Title</FormLabel>
-            <Typography>This title will be shown at the top of your event page.</Typography>
+          <FormControl>
+            <FormLabel 
+            sx={{color: "black", fontFamily: "sans-serif", fontWeight: "bold", fontSize: "17px"}}
+            >
+              Event Title
+            </FormLabel>
+            <Typography sx={{color: "gray", mt: "5px"}}>This title will be shown at the top of your event page.</Typography>
             <TextField
                 placeholder="Example: Birthday Party, Friends Reunion"
                 name="event_title"
                 value={eventInfo.event_title}
                 onChange={handleChange}
                 fullWidth
+                sx={{mt: "20px"}}
             />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel 
+            sx={{color: "black", fontFamily: "sans-serif", fontWeight: "bold", fontSize: "17px"}}
+            >
+              Description
+            </FormLabel>
+            <Typography sx={{color: "gray", mt: "5px"}}>Give attendees a short overview of what your event is about.</Typography>
+            <TextField
+              placeholder="Describe about your event briefly."
+              name="description"
+              value={eventInfo.description}
+              onChange={handleChange}
+              multiline
+              rows={3}
+              fullWidth
+              sx={{mt: "20px"}}
+            />
+          </FormControl>
+
+
+         <FormControl>
+            <FormLabel 
+            sx={{color: "black", fontFamily: "sans-serif", fontWeight: "bold", fontSize: "17px"}}
+            >
+              Location
+            </FormLabel>
+            <TextField
+              placeholder="Search Location"
+              // name="location"
+              inputRef={inputRef}
+              // value={eventInfo.location.address}
+              // onChange={handleChange}
+              fullWidth
+              sx={{mt: "10px"}}
+            />
+          </FormControl>
+
+          {/* --------- Google Map -------- */}
+          <APIProvider 
+          apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}
+          libraries={["places"]}
+          >
+            <Box sx={{height: "300px", width: "100%"}}>
+              <Map 
+              ref={mapRef}
+              defaultCenter={defaultLocationMarker}
+              defaultZoom={8}
+              mapId={import.meta.env.VITE_MAP_ID}
+              onClick={(e) => {
+                const lat = e.detail.latLng.lat;
+                const lng = e.detail.latLng.lng;
+
+                setDefaultLocationMarker({ lat, lng });
+               
+                setEventInfo((prev) => ({
+                  ...prev,
+                  location: {
+                    ...prev.location,
+                    latitude: lat,
+                    longitude: lng,
+                  },
+                }));
+              }}
+              >
+                <AdvancedMarker
+                position={defaultLocationMarker}
+                draggable
+                onDragEnd={(e) => {
+                  const lat = e.latLng.lat();
+                  const lng = e.latLng.lng();
+
+                  setDefaultLocationMarker({ lat, lng });
+                  
+                  setEventInfo((prev) => ({
+                    ...prev,
+                    location: {
+                      ...prev.location,
+                      latitude: lat,
+                      longitude: lng,
+                    },
+                  }));
+                }}
+              >
+                <Pin background="white" borderColor="purple" glyphColor={"purple"} />
+              </AdvancedMarker>
+                  
+              </Map>
+              
+
+            </Box>
+            
+
+          </APIProvider>
+
+          {/* {eventInfo.location.address && (
+            <Typography sx={{ fontSize: 13, color: "gray" }}>
+              Selected: {eventInfo.location.address}
+            </Typography>
+          )} */}
+         
+
           
 
-          <FormLabel sx={{color: "black"}}>Description</FormLabel>
-          <Typography>Give attendees a short overview of what your event is about.</Typography>
-          <TextField
-            placeholder="Describe about your event briefly."
-            name="description"
-            value={eventInfo.description}
-            onChange={handleChange}
-            multiline
-            rows={3}
-            fullWidth
-          />
-
-          <TextField
-            label="Location"
-            name="location"
-            value={eventInfo.location}
-            onChange={handleChange}
-            fullWidth
-          />
-
+          <FormControl>
+          <FormLabel sx={{color: "black", fontFamily: "sans-serif", fontWeight: "bold", fontSize: "16px"}}>Date</FormLabel>
           <TextField
             type="date"
             // label="Start Date"
@@ -99,33 +235,32 @@ function ScratchCreation() {
             onChange={handleChange}
             
           />
+          </FormControl>
 
-          <TextField
-            type="date"
-            // label="End Date"
-            name="end_date"
-            value={eventInfo.end_date}
-            onChange={handleChange}
-            
-          />
+          
+          <FormControl>
+            <FormLabel sx={{color: "black", fontFamily: "sans-serif", fontWeight: "bold", fontSize: "16px"}}>Start Time</FormLabel>
+            <TextField
+              type="time"
+              
+              name="start_time"
+              value={eventInfo.start_time}
+              onChange={handleChange}
+              
+            />
+          </FormControl>
 
-          <TextField
-            type="time"
-            // label="Start Time"
-            name="start_time"
-            value={eventInfo.start_time}
-            onChange={handleChange}
-            
-          />
-
-          <TextField
-            type="time"
-            // label="End Time"
-            name="end_time"
-            value={eventInfo.end_time}
-            onChange={handleChange}
-            
-          />
+          <FormControl>
+            <FormLabel sx={{color: "black", fontFamily: "sans-serif", fontWeight: "bold", fontSize: "16px"}}>End Time</FormLabel>
+            <TextField
+              type="time"
+              // label="End Time"
+              name="end_time"
+              value={eventInfo.end_time}
+              onChange={handleChange}
+              
+            />
+          </FormControl>
 
           <TextField
             label="Event Image URL"
@@ -134,15 +269,7 @@ function ScratchCreation() {
             onChange={handleChange}
             fullWidth
           />
-
-          <TextField
-            label="Slug"
-            name="slug"
-            value={eventInfo.slug}
-            onChange={handleChange}
-            fullWidth
-          />
-
+         
           
           <Button type="submit" variant="contained">
             Create Event
